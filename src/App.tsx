365150ea5c77bc200acb347aea24fd8c4a534b5c
Prakalyaa <prakalyam14@gmail.com>
2025-08-24 00:10:45 +0530

    feat: add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
+import { Navigate, Route, Routes } from "react-router-dom"
+import { CharactersPage } from "./pages/CharacterPage"
 
 function App() {
 
   return (
     <>
-    
+    <Routes>
+      <Route path="/" element={<Navigate to="/characters" replace />} />
+      <Route path="/characters" element={<CharactersPage />} />
+      <Route path="/character/:id" element={<CharactersPage />} />
+    </Routes>
     </>
   )
 }
